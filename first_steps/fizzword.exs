b7eb61ddfde1536c57b2a4e074691a9cb0828059
Patrício dos Santos <eu@patricio.me>
2018-04-07 22:01:57 +0100

    Exercise: Functions-3

--- first_steps/fizzword.exs
+++ first_steps/fizzword.exs
@@ -6,7 +6,18 @@ fizzword = fn
   (_, _, c) -> c
 end
 
-IO.puts fizzword.(0, 0, 0)
-IO.puts fizzword.(0, 9, 9)
-IO.puts fizzword.(8, 0, 9)
-IO.puts fizzword.(8, 8, 9)
+# Exercise: Functions-3
+fizz = fn(n) -> fizzword.(rem(n,3), rem(n,5), n) end
+
+#IO.puts fizzword.(0, 0, 0)
+#IO.puts fizzword.(0, 9, 9)
+#IO.puts fizzword.(8, 0, 9)
+#IO.puts fizzword.(8, 8, 9)
+
+IO.puts fizz.(10)
+IO.puts fizz.(11)
+IO.puts fizz.(12)
+IO.puts fizz.(13)
+IO.puts fizz.(14)
+IO.puts fizz.(15)
+IO.puts fizz.(16)
